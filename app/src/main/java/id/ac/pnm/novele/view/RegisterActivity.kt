627d75909da113ebea40a6e1ac65b6f005f34112
fathursn21 <faturrosin333@gmail.com>
2025-11-27 13:40:56 +0700

    feat: register scroll view

---
+++ app/src/main/java/id/ac/pnm/novele/view/RegisterActivity.kt
@@ -0,0 +1,22 @@
+package id.ac.pnm.novele.view
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import id.ac.pnm.novele.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_register)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.container)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+    }
+}
